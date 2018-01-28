c9f86899f6b84d4e1a3f8fa3eba1ee98137c6cae
Eclipse <godzplayer@hotmail.com>
2018-01-28 16:05:30 +0200

    Update main.cpp

--- Pbinfo/377/main.cpp
+++ Pbinfo/377/main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-unsigned int n;
-unsigned int divs[100000], counter;
-
-void Divisors(unsigned int a) {
-    unsigned int i;
-    float r = sqrt(i);
-    for (i = 1; i <= r; i++) {
-        if (a % i == 0) {
-            counter++;
-            divs[counter] = i;
-            counter++;
-            divs[counter] = a / i;
-        }
-    }
-}
-
-
-
 int main()
 {
+    int n;
     cin >> n;
-    Divisors(n);
-    sort(divs + 1, divs + 1 + counter);
-    cout << divs[counter / 2] << " " << divs[counter / 2 + 1];
-    return 0;
+    vector<int> divisors;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            divisors.push_back(n / i);
+        }
+    }
+    sort(divisors.begin(), divisors.end());
+    if (divisors.size() % 2 == 1) {
+        cout << divisors[divisors.size() / 2];
+    } else {
+        cout << divisors[divisors.size() / 2 - 1] << " " << divisors[divisors.size() / 2];
+    }
 }
